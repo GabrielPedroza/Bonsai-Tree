1a380650127390092fa014fc64c4ff534584cc25
Gabriel Pedroza <thegabrielpedroza@gmail.com>
2021-11-30 19:47:34 -0500

    navbar responsive in respect to phone and tablet

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,26 +1,40 @@
-import React from "react"
+import React, { useState } from "react"
 import "./navbar.scss"
 import logo from "../../../public/bonsai__logo-removebg-preview.png"
+import MenuIcon from "@mui/icons-material/Menu"
+import CloseIcon from "@mui/icons-material/Close"
 
 const Navbar = () => {
+	const [isOpen, setIsOpen] = useState(false)
 	return (
-		<div className='navbar'>
-			<div className='navbar__logo'>
+		<div className='c-navbar'>
+			<div className='c-navbar__logo'>
 				<img src={logo} alt='bonsai tree logo' />
 			</div>
-			<div className='navbar__links'>
+			<div className='c-navbar__links'>
 				<a href='#home'>Home</a>
 				<a href='#history'>History</a>
 				<a href='#benefits'>Benefits</a>
 				<a href='#testimonials'>Testimonials</a>
-				<div className='navbar__links__buttons'>
-					<button className='navbar__links__buttons--primary'>
-						<a href='#signup'>Sign up</a>
-					</button>
-					<button className='navbar__links__buttons--accent'>
-						<a href='#signin'>Sign in</a>
-					</button>
-				</div>
+			</div>
+			<div className='c-navbar__buttons'>
+				<a
+					className='c-navbar__buttons c-navbar__buttons--signup'
+					href='#signup'>
+					Sign up
+				</a>
+				<a
+					className='c-navbar__buttons c-navbar__buttons--signin'
+					href='#signin'>
+					Sign in
+				</a>
+			</div>
+			<div className='js-navbar__menu'>
+				{isOpen ? (
+					<CloseIcon onClick={() => setIsOpen(false)} />
+				) : (
+					<MenuIcon onClick={() => setIsOpen(true)} />
+				)}
 			</div>
 		</div>
 	)
