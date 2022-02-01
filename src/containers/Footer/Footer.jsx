47752f95ce4669f8f27929d6567bfd9acafc4554
Gabriel Pedroza <thegabrielpedroza@gmail.com>
2022-02-01 14:06:35 -0500

    finished footer

--- src/containers/Footer/Footer.jsx
+++ src/containers/Footer/Footer.jsx
@@ -1,8 +1,52 @@
 import React from "react"
-// import "./footer.scss"
+import "./footer.scss"
 
 const Footer = () => {
-	return <div>Footer</div>
+	return (
+		<div className='l-footer__container'>
+			<div className='c-footer__phone-number'>
+				Questions? Call <span className='c-phone-number'>1-800-456-7890</span>
+			</div>
+			<div className='l-footer__info'>
+				<a className='c-footer__info--hover' href='#faq'>
+					FAQ
+				</a>
+				<a className='c-footer__info--hover' href='#account'>
+					Account
+				</a>
+				<a className='c-footer__info--hover' href='#privacy-policy'>
+					Privacy Policy
+				</a>
+				<a className='c-footer__info--hover' href='#terms'>
+					Terms
+				</a>
+				<a className='c-footer__info--hover' href='#contact-us'>
+					Contact Us
+				</a>
+				<a className='c-footer__info--hover' href='#help-center'>
+					Help Center
+				</a>
+				<a className='c-footer__info--hover' href='#corporate-information'>
+					Corporate Information
+				</a>
+				<a className='c-footer__info--hover' href='#careers'>
+					Careers
+				</a>
+				<a className='c-footer__info--hover' href='#advertise'>
+					Advertise
+				</a>
+				<a className='c-footer__info--hover' href='#legal-notice'>
+					Legal Notice
+				</a>
+				<a className='c-footer__info--hover' href='#buy-gift-cards'>
+					Buy Gift cards
+				</a>
+				<a className='c-footer__info--hover' href='#redeem-gift-cards'>
+					Redeem Gift cards
+				</a>
+			</div>
+		</div>
+	)
 }
 
 export default Footer
